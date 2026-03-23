af6edd6046f7036dd5ef8acae6fafc344db0edfd
Hansini Chetan Dhameliya <hansini.dhameliya2@gmail.com>
2026-03-23 15:31:23 -0400

    Create: migration for songs table

---
+++ backend/database/migrations/2026_03_23_164913_create_songs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('artist_id')->nullable();
+            $table->bigInteger('genre_id')->nullable();
+            $table->deciaml('price', 8, 2);
+            $table->string('thumbnail')->nullable();
+            $table->string('thumbnail_alt')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
