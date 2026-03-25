3ed043033da5a7867cf6abecaab623c4a9831b04
Hansini Chetan Dhameliya <hansini.dhameliya2@gmail.com>
2026-03-25 11:23:36 -0400

    Update: DatabaseSeeder to register SongsSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             ArtistsSeeder::class,
-            GenresSeeder::class
+            GenresSeeder::class,
+            SongsSeeder::class
         ]);
 
     }
