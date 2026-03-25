0c8cc19fac32165256216282cfc2cda6e6a51ab2
Hansini Chetan Dhameliya <hansini.dhameliya2@gmail.com>
2026-03-25 10:55:16 -0400

    Create: songs model and populate it

---
+++ backend/app/Models/Songs.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Songs extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'artist_id',
+        'genre_id',
+        'price',
+        'thumbnail',
+        'thumbnail_alt'
+    ];
+
+    // relationships
+    // each songs belongs to one artist and one genre
+    public function artist() {
+        return $this->belongsTo(Artists::class);
+    }
+
+    public function genre() {
+        return $this->belongsTo(Genres::class);
+    }
+}
