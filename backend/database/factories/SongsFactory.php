e254bad2058966d2a66b55d8c22337ff2f5991d2
Hansini Chetan Dhameliya <hansini.dhameliya2@gmail.com>
2026-03-25 11:09:26 -0400

    Create: SongsFactory and add logica to populate the table

---
+++ backend/database/factories/SongsFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Artists;
+use App\Models\Genres;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SongsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            // creates random 3 word sentences as names of songs
+            'name' => $this->faker->sentence(3),
+            // shuffles the artists entires and picks their ids
+            'artist_id' => Artists::inRandomOrder()->value('id'),
+            // same as artists
+            'genre_id' => Genres::inRandomOrder()->value('id'),
+            // creates random 2 digit float numbers between the specified range
+            'price' => $this->faker->randomFloat(2, 0.99, 19.99),
+            // null because to be entered manually
+            'thumbnail' => null,
+            'thumbnail_alt' => null
+        ];
+    }
+}
